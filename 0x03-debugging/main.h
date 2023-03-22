5f10540481496b2e61b7bc0754bd1732d80c3b8c
Abstaina44 <padcliff88@gmail.com>
2023-03-22 02:03:22 -0700

    0 Task

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,7 @@
+int largest_number(int a,  int b,  int c);
+Void print_remaining_days(int month,  int day,  int year);
+int  convert_day(int month. Int day);
+Void print _times_table(int n);
+
+#endif
+
