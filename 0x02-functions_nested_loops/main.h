5417a4d0ee07e1b48afece65325c0abc2968c8cf
Abstaina44 <padcliff88@gmail.com>
2023-03-23 16:18:17 -0700

    3-print_numbers.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,3 +17,5 @@ void print_to_98(int n);
 void print_times_table(int n);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+
+#endif
