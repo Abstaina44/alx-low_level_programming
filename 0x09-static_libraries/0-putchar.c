8194c978a9d4aa448af186e12c6aac2af7f14d20
Abstaina44 <padcliff88@gmail.com>
2023-04-09 12:17:08 -0700

    static library libmy.a containing all the functions

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -11,3 +12,4 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
