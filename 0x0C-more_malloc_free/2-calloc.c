5f23a42678c0b4c5936ae962df29631980cb5a6f
Abstaina44 <padcliff88@gmail.com>
2023-04-13 07:17:08 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,51 +2,31 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - ...
- * @s1: ...
- * @s2: ...
- * @n: ...
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of members in the array
+ * @size: size in bytes
  *
- * Return: ...
+ * Return: void pointer to allocated memory
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i = 0, j = 0, k = 0, l = 0;
-	char *str;
+	int i = 0, l = 0;
+	char *p;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[i])
-		i++;
-
-	while (s2[k])
-		k++;
-
-	if (n >= k)
-		l = i + k;
-	else
-		l = i + n;
-
-	str = malloc(sizeof(char) * l + 1);
-	if (str == NULL)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	k = 0;
-	while (j < l)
-	{
-		if (j <= i)
-			str[j] = s1[j];
+	l = nmemb * size;
+	p = malloc(l);
 
-		if (j >= i)
-		{
-			str[j] = s2[k];
-			k++;
-		}
-		j++;
+	if (p == NULL)
+		return (NULL);
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
 	}
-	str[j] = '\0';
-	return (str);
+
+	return (p);
 }
