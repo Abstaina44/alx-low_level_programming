c500ec5f0c8eee1af2fb5d4d6c0a6ecd03dd23b9
Abstaina44 <padcliff88@gmail.com>
2023-04-19 19:32:09 -0700

    3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,13 @@
-#ifndef calc_h
-#define calc_h
+#ifndef CALC_H
+#define CALC_H
+
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * struct op - Struct op
  *
@@ -12,10 +20,4 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 #endif
