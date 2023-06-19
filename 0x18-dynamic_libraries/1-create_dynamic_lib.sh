a1b5ea1b9ed100a6ae19981671c7649d324aa98e
Abstaina44 <padcliff88@gmail.com>
2023-06-19 10:25:23 -0700

    Dynamic library libdynamic.so

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -c -fpic *.c
-gcc -shared *.o -o liball.so
-
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,5 @@
-
-#ifndef H
-#define H
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _islower(int c);
@@ -22,5 +21,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* H */
+#endif /*MAIN_H*/
