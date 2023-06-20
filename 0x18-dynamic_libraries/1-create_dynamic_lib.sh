93f66f7437020efbf7bf79a12d7403be767dd087
Abstaina44 <padcliff88@gmail.com>
2023-06-20 15:24:16 -0700

    Updated 1-create_dynamic_lib.sh

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.c -c -fPIC
-gcc *.o -shared -o liball.so
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.c -c -fPIC
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 *.o -shared -o liball.so
