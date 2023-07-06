3312d3d605ce4e43fe1f7f77d8fb74a9200ee0c5
Abstaina44 <padcliff88@gmail.com>
2023-07-06 11:52:40 -0700

    A Function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,50 +1,42 @@
-#include <stdio.h>
+#include "hash_tables.h"
 #include <stdlib.h>
 
-typedef struct hash_table {
-    unsigned long int size;
-    // Additional fields can be added based on your requirements
-    // For simplicity, let's assume we only need an array in this example
-    int *array;
-} hash_table_t;
+/**
+ * hash_table_create - create a new hash table
+ * @size: size of the hash table to be created
+ * Return: Pointer to the newly created hash table or NULL
+ */
 
-hash_table_t *hash_table_create(unsigned long int size) {
-    hash_table_t *new_table = malloc(sizeof(hash_table_t));
-    if (new_table == NULL) {
-        return NULL; // Return NULL if memory allocation failed
-    }
 
-    new_table->size = size;
-    new_table->array = malloc(size * sizeof(int));
-    if (new_table->array == NULL) {
-        free(new_table); // Cleanup allocated memory
-        return NULL; // Return NULL if memory allocation failed
-    }
+hash_table_t *hash_table_create(unsigned long size)
+{
+	hash_table_t *map;
+	unsigned long i = 0;
 
-    // Initialize array elements to some default value if needed
-    for (unsigned long int i = 0; i < size; i++) {
-        new_table->array[i] = 0; // Example: Initializing all elements to 0
-    }
+	if (size <= 0)
+		return (NULL);
 
-    return new_table;
+	map = malloc(sizeof(*map));
+
+	if (!map)
+	{
+		free(map);
+		map = NULL;
+		return (map);
+	}
+
+	map->size = size;
+	map->array = malloc(sizeof(map->array) * map->size);
+
+	if (!(map->array))
+	{
+		free(map->array);
+		map->array = NULL;
+		return (NULL);
+	}
+
+	while (i < size)
+		map->array[i++] = NULL;
+
+	return (map);
 }
-
-int main() {
-    unsigned long int table_size = 10;
-    hash_table_t *table = hash_table_create(table_size);
-
-    if (table == NULL) {
-        printf("Failed to create hash table\n");
-        return 1;
-    }
-
-    printf("Hash table created successfully\n");
-
-    // You can perform operations on the hash table here
-
-    free(table->array);
-    free(table);
-
-    return 0;
-}
-
